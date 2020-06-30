2236be50c65dcd3236885985596c484c25f9bfb4
“Paul <pduvall@gmail.com>
2020-06-30 15:55:51 +0000

    call Sessions with SAM example

--- src/PreTraffic.js
+++ src/PreTraffic.js
@@ -1,76 +1,66 @@
-// @ts-check
-'use strict';
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+// //
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so.
+// //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 const aws = require('aws-sdk');
-const codedeploy = new aws.CodeDeploy({apiVersion: '2014-10-06'});
-var lambda = new aws.Lambda();
+const codedeploy = new aws.CodeDeploy();
+const lambda = new aws.Lambda();
+
+exports.lambdaHandler = async (event) => {
+  let status = 'Failed';
+
+  console.log("Entering PreTraffic Hook!");
+  console.log(JSON.stringify(event));
+
+  //Read the DeploymentId from the event payload.
+  let deploymentId = event.DeploymentId;
+  console.log("deploymentId=" + deploymentId);
+
+  //Read the LifecycleEventHookExecutionId from the event payload
+  let lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
+  console.log("lifecycleEventHookExecutionId=" + lifecycleEventHookExecutionId);
 
 
-exports.handler = async (event, context, callback) => {
+  let invokeParams = {
+    FunctionName: process.env.CurrentVersion
+  }
 
-    console.log("Entering PreTraffic Hook!");
-    console.log(JSON.stringify(event));
+  try{
+    let returnEvent = await lambda.invoke(invokeParams).promise()
+    console.log(returnEvent);
+    console.log(JSON.parse(returnEvent.Payload))
+    if(JSON.parse(returnEvent.Payload).preTest) status = 'Succeeded';
+  } catch (err) {
+    console.log('error invoking Lambda');
+    console.log(err)
+  }
 
-    //Read the DeploymentId from the event payload.
-    let deploymentId = event.DeploymentId;
-    console.log("deploymentId=" + deploymentId);
+  // Prepare the validation test results with the deploymentId and
+  // the lifecycleEventHookExecutionId for AWS CodeDeploy.
+  let params = {
+    deploymentId: deploymentId,
+    lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
+    status: status // status can be 'Succeeded' or 'Failed'
+  };
 
-    //Read the LifecycleEventHookExecutionId from the event payload
-    let lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
-    console.log("lifecycleEventHookExecutionId=" + lifecycleEventHookExecutionId);
-
-    var functionToTest = process.env.CurrentVersion;
-    console.log("Testing new function version: " + functionToTest);
-
-    // Perform validation of the newly deployed Lambda version
-    var lambdaParams = {
-        FunctionName: functionToTest,
-        InvocationType: "RequestResponse"
-    };
-
-    var lambdaResult = "Failed";
-    lambda.invoke(lambdaParams, function (err, data) {
-        if (err) {	// an error occurred
-            console.log(err, err.stack);
-            lambdaResult = "Failed";
-        }
-        else {	// successful response
-            var result = JSON.parse(data.Payload);
-            console.log("Result: " + JSON.stringify(result));
-
-            // Check the response for valid results
-            // The response will be a JSON payload with statusCode and body properties. ie:
-            // {
-            //		"statusCode": 200,
-            //		"body": 51
-            // }
-            if (result.body == 1) {
-                lambdaResult = "Succeeded";
-                console.log("Validation testing succeeded!");
-            }
-            else {
-                lambdaResult = "Failed";
-                console.log("Validation testing failed!");
-            }
-
-            // Complete the PreTraffic Hook by sending CodeDeploy the validation status
-            var params = {
-                deploymentId: deploymentId,
-                lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
-                status: lambdaResult // status can be 'Succeeded' or 'Failed'
-            };
-            
-            console.log("lambdaResult=" + lambdaResult);
-            try {
-              codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
-              console.log("putLifecycleEventHookExecutionStatus done. executionStatus=[" + params.status + "]");
-              return 'Validation test succeeded'
-            }
-            catch (err) {
-              console.log("putLifecycleEventHookExecutionStatus ERROR: " + err);
-              throw new Error('Validation test failed')
-            }
-        }
-        
-    });
+  try {
+    await codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
+    console.log("putLifecycleEventHookExecutionStatus done. executionStatus=[" + params.status + "]");
+    return 'Validation test succeeded'
+  } catch (err) {
+    console.log("putLifecycleEventHookExecutionStatus ERROR: " + err);
+    throw new Error('Validation test failed')
+  }
 }
