87ab9541405486e622e358b6629d3fc14578fd8f
Paul Duvall <pduvall@gmail.com>
2020-07-06 20:42:51 +0000

    iterate ddb table 1642

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,17 +2,11 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: process.env.REGION});
 const htmlResponse = require('./html-response');
 
-    console.log('1625 Running app.js');
+var params = {
+  TableName: process.env.TABLE_NAME,
+};
 
-
-    let scanningParameters = {
-        TableName: process.env.TABLE_NAME,
-        Limit: 100 //maximum result of 100 items
-    };
-
-
-
-docClient.scan(scanningParameters, onScan);
+docClient.scan(params, onScan);
 var count = 0;
 
 function onScan(err, data) {
@@ -23,59 +17,12 @@ function onScan(err, data) {
         data.Items.forEach(function(itemdata) {
            console.log("Item :", ++count,JSON.stringify(itemdata));
         });
+
+        // continue scanning if we have more items
+        if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning for more...");
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            docClient.scan(params, onScan);
+        }
     }
 }
-    
-const ddb = `
-  <html>
-  <head>
-    <meta charset="utf-8"/>
-  </head>
-  <body>
-    <form method="POST">
-      Please enter your name:
-      <input type="text" name="name"/>
-      <br/>
-      <input type="submit" />
-    </form>
-  </body>
-  </html>
-`;
-
-const formHtml = `
-  <html>
-  <head>
-    <meta charset="utf-8"/>
-  </head>
-  <body>
-    <form method="POST">
-      Please enter your name:
-      <input type="text" name="name"/>
-      <br/>
-      <input type="submit" />
-    </form>
-  </body>
-  </html>
-`;
-
-const thanksHtml = `
-  <html>
-  <head>
-    <meta charset="utf-8"/>
-  </head>
-  <body>
-    <h1>Thanks</h1>
-    <p>We received your submission</p>
-  </body>
-  </html>
-`;
-
-exports.lambdaHandler = async (event, context) => {
-  console.log(JSON.stringify(event, null, 2));
-
-  if (event.httpMethod === 'GET') {
-    return htmlResponse(formHtml);
-  } else {
-    return htmlResponse(thanksHtml);
-  }
-};
